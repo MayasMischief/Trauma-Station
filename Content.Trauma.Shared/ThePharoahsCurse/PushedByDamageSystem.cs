1b379f33f9dc204c3b3d61b424d10a1406a413c9
MayasMischief <anusholeripper73@yahoo.com>
2026-06-01 20:14:54 -0700

    wawa

--- Content.Trauma.Shared/ThePharoahsCurse/PushedByDamageSystem.cs
+++ Content.Trauma.Shared/ThePharoahsCurse/PushedByDamageSystem.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Throwing;
 using Content.Shared.Weapons.Melee.Events;
 
 namespace Content.Trauma.Shared.ThePharoahsCurse;
@@ -7,15 +8,21 @@ namespace Content.Trauma.Shared.ThePharoahsCurse;
 /// </summary>
 public sealed partial class PushedByDamageSystem : EntitySystem
 {
+    [Dependency] private ThrowingSystem _throwing = default!;
+    [Dependency] private SharedTransformSystem _transform = default!;
     public override void Initialize()
     {
-        SubscribeLocalEvent<PushedByDamageComponent, MeleeHitEvent>(OnMeleeHit);
-        //SubscribeLocalEvent<PushedByDamageComponent, ProjectileHitEvent>(OnProjectileHit);
-        /// no hitscans they're slop
+        SubscribeLocalEvent<PushedByDamageComponent, AttackedEvent>(OnMeleeHit);
+        //SubscribeLocalEvent<PushedByDamageComponent, GotHitByProjectileEvent>(OnProjectileHit);
+        // hitscans are slop
     }
 
-    private void OnMeleeHit(Entity<PushedByDamageComponent> ent, ref MeleeHitEvent args)
+    private void OnMeleeHit(Entity<PushedByDamageComponent> ent, ref AttackedEvent args)
     {
-        Log.Debug($"{args}");
+        var userPos = _transform.GetMapCoordinates(args.User).Position;
+        var entPos = _transform.GetMapCoordinates(ent).Position;
+        var throwDir = userPos - entPos;
+        _throwing.TryThrow(ent, throwDir * -1);
     }
+
 }
